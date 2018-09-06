5985175b00e72a49011538642af46855ab8cbee7
Kaitlin Bunn <kabunn1991@gmail.com>
2018-09-06 14:31:23 -0400

    spelling error

---
+++ search-drills.js
@@ -0,0 +1,5 @@
+// Find a Book
+// Imagine you are looking for a book in a library with a Dewey Decimal index. How would you go about it? Can you express this process as a searching algorithm?
+// 10 categories with sub categories
+
+// A binary search would be good for searching through this collection of books based on how they are organized. With a binary search, you could eliminate categories first and then eliminate the sub categories until you found the correct one that houses the book you're looking for.
